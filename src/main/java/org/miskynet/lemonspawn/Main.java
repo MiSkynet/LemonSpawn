4c4c3d00839c075c67934be2702bdd7ae8f53251
MiSkynet <michele.sauer.2607@gmail.com>
2024-10-07 19:54:02 +0200

    First commit

---
+++ src/main/java/org/miskynet/lemonspawn/Main.java
@@ -0,0 +1,17 @@
+package org.miskynet.lemonspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
